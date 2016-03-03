c097235fb00c07896ea96e58a33dd829e258ce24
Nick Hu <nick@nickhu.info>
2016-03-03 03:12:38 +0000

    clean up

--- server.js
+++ server.js
@@ -1,33 +1,24 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/public/index.html');
+});
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+app.post('/api/fileanalyse/', upload.single('file'), function (req, res, next) {
+        res.send({filesize: req.file.size});
+    }
+);
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
 
 var port = process.env.PORT || 8080;
+
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
